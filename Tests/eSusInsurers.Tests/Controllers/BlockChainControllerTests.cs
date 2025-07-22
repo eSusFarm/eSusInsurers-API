c941722d31d4d8615342042fbed42728ae44a9c6
sdumisile-mbambo <sdumisilembambo@gmail.com>
2025-07-23 01:33:31 +0200

    SM | #80 | #86 | #87 | #81 | #84

--- Tests/eSusInsurers.Tests/Controllers/BlockChainControllerTests.cs
+++ Tests/eSusInsurers.Tests/Controllers/BlockChainControllerTests.cs
@@ -1,4 +1,5 @@
 using eSusInsurers.Domain.Entities;
+using eSusInsurers.Models.Etherisc;
 using eSusInsurers.Models.Etherisc.Policy;
 using eSusInsurers.Services.Interfaces;
 using FluentAssertions;
@@ -34,11 +35,106 @@ public class BlockChainControllerTests
         Assert.Equal(expectedResponse, returnedResponse);
     }
     [Fact]
-    public async Task BlockChainController_ThrowsException_CreatesBlockSuccessfully()
+    public async Task BlockChainController_ThrowsException_ReturnsBadRequest()
     {
         var expectedException = new Exception("Test error");
         _mockEtheriscService.Setup(s=>s.AddPolicy(It.IsAny<AddPolicyRequest>(), It.IsAny<CancellationToken>())).Throws(expectedException);
         var result =  _controller.AddPolicy(new AddPolicyRequest(), new CancellationToken());
+        var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        var error = badRequestResult.Value.Should().BeAssignableTo<object>().Subject;
+        error.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task CreatePlolicy_CreateBlock_CreatesBlockSuccessfully()
+    {
+        var expectedResponse = new AddPolicyResponse
+        {
+            cropInsuranceId = 1,
+            message = "test"
+        };
+        _mockEtheriscService.Setup(s=>s.CreateBlockChainPolicy(It.IsAny<AddPolicyRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResponse);
+        var result =  _controller.AddBlockChainPolicy(new AddPolicyRequest(), new CancellationToken());
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returnedResponse = Assert.IsType<AddPolicyResponse>(okResult.Value);
+        Assert.Equal(expectedResponse, returnedResponse);
+    }
+    [Fact]
+    public async Task CreatePlolicy_ThrowsException_ReturnsBadRequest()
+    {
+        var expectedException = new Exception("Test error");
+        _mockEtheriscService.Setup(s=>s.CreateBlockChainPolicy(It.IsAny<AddPolicyRequest>(), It.IsAny<CancellationToken>())).Throws(expectedException);
+        var result =  _controller.AddBlockChainPolicy(new AddPolicyRequest(), new CancellationToken());
+        var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        var error = badRequestResult.Value.Should().BeAssignableTo<object>().Subject;
+        error.Should().NotBeNull();
+    }
+    [Fact]
+    public async Task CreatePlolicyMetaData_CreateBlock_CreatesBlockSuccessfully()
+    {
+        var expectedResponse = new AddPolicyResponse
+        {
+            cropInsuranceId = 1,
+            message = "test"
+        };
+        _mockEtheriscService.Setup(s=>s.AddPolicyMetadata(It.IsAny<AddPolicyRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResponse);
+        var result =  _controller.AddPolicyMetaData(new AddPolicyRequest(), new CancellationToken());
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returnedResponse = Assert.IsType<AddPolicyResponse>(okResult.Value);
+        Assert.Equal(expectedResponse, returnedResponse);
+    }
+    [Fact]
+    public async Task CreatePlolicyMetaData_ThrowsException_ReturnsBadRequest()
+    {
+        var expectedException = new Exception("Test error");
+        _mockEtheriscService.Setup(s=>s.AddPolicyMetadata(It.IsAny<AddPolicyRequest>(), It.IsAny<CancellationToken>())).Throws(expectedException);
+        var result =  _controller.AddPolicyMetaData(new AddPolicyRequest(), new CancellationToken());
+        var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        var error = badRequestResult.Value.Should().BeAssignableTo<object>().Subject;
+        error.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task? UpdateInsuranceRick_CreateBlock_CreatesBlockSuccessfully()
+    {
+        var expectedResponse = new UpdateRiskResponse
+        {
+            configId = "1",
+            crop = "test"
+        };
+        _mockEtheriscService.Setup(s=>s.UpdateRisk(It.IsAny<UpdateRiskRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResponse);
+        var result =  _controller.UpdateInsuranceRisk(new UpdateRiskRequest(), new CancellationToken());
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returnedResponse = Assert.IsType<UpdateRiskResponse>(okResult.Value);
+        Assert.Equal(expectedResponse, returnedResponse);
+    }
+    [Fact]
+    public async Task UpdateInsuranceRick_ThrowsException_ReturnsBadRequest()
+    {
+        var expectedException = new Exception("Test error");
+        _mockEtheriscService.Setup(s=>s.UpdateRisk(It.IsAny<UpdateRiskRequest>(), It.IsAny<CancellationToken>())).Throws(expectedException);
+        var result =  _controller.UpdateInsuranceRisk(new UpdateRiskRequest(), new CancellationToken());
+        var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        var error = badRequestResult.Value.Should().BeAssignableTo<object>().Subject;
+        error.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task? SyncInsuranceRick_CreateBlock_CreatesBlockSuccessfully()
+    {
+        var expectedResponse = "done";
+        _mockEtheriscService.Setup(s=>s.SyncRisk(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResponse);
+        var result =  _controller.SyncInsuranceRisk("test", new CancellationToken());
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returnedResponse = Assert.IsType<string>(okResult.Value);
+        Assert.Equal(expectedResponse, returnedResponse);
+    }
+    [Fact]
+    public async Task SyncInsuranceRick_ThrowsException_ReturnsBadRequest()
+    {
+        var expectedException = new Exception("Test error");
+        _mockEtheriscService.Setup(s=>s.SyncRisk(It.IsAny<string>(), It.IsAny<CancellationToken>())).Throws(expectedException);
+        var result =  _controller.SyncInsuranceRisk("test", new CancellationToken());
         var badRequestResult = result.Result.Should().BeOfType<BadRequestObjectResult>().Subject;
         var error = badRequestResult.Value.Should().BeAssignableTo<object>().Subject;
         error.Should().NotBeNull();
